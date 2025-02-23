1f666811ef96bd2eb3cdd020a5abcea9a2f8477c
TobiasPottier <Tobias.pottier@student.howest.be>
2025-02-23 23:22:50 +0100

    User now able to login

--- Models/User.js
+++ Models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { 
@@ -21,5 +22,12 @@ const userSchema = new mongoose.Schema({
   },
   watchedMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 }, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 module.exports = mongoose.model('User', userSchema);
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../Models/user.js');
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Create a new user
 router.post('/register', async (req, res) => {
@@ -10,6 +13,36 @@ router.post('/register', async (req, res) => {
     res.status(201).json(newUser);
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+});
+
+// Login user
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ msg: 'Invalid credentials' });
+    }
+
+    // Compare given password with hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ msg: 'Invalid credentials' });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      JWT_SECRET,
+    );
+    // { expiresIn: '1h' }
+
+    res.json({ token, msg: 'Login successful' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
